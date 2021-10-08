b77503476e06e88de026dae19dcc0dffe56bd860
jaybabel <BabelJayE@JohnDeere.com>
2021-10-08 14:21:43 -0500

    tabs seeder file

--- seeders/20211008002131-demo-tabs.js
+++ seeders/20211008002131-demo-tabs.js
@@ -2,15 +2,70 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('tabs', 
+    [
+      {
+        tabTitle: 'Amazing Grace',
+        tabURL: '/tabs/amazing-grace-gtm.pdf',
+        tuning: '1',
+        difficulty: 'Easy',
+        artist: '',
+        genre: '',
+        tabSource: '',
+        rating: '',
+      },
+      {
+        tabTitle: 'Everything I Own',
+        tabURL: '/tabs/everything_i_own_tab.pdf',
+        tuning: '1',
+        difficulty: '',
+        artist: '',
+        genre: '',
+        tabSource: '',
+        rating: '',
+      },
+      {
+        tabTitle: 'Father and Son',
+        tabURL: '/tabs/father_and_son_tab.pdf',
+        tuning: '1',
+        difficulty: '',
+        artist: '',
+        genre: '',
+        tabSource: '',
+        rating: '',
+      },
+      {
+        tabTitle: 'House of the Rising Sun',
+        tabURL: '/tabs/house-of-the-rising-sun-gtm.pdf',
+        tuning: '1',
+        difficulty: 'Easy',
+        artist: '',
+        genre: '',
+        tabSource: '',
+        rating: '',
+      },
+      {
+        tabTitle: 'Mr. Bojangles',
+        tabURL: '/tabs/mr_bojangles_tab.pdf',
+        tuning: '1',
+        difficulty: '',
+        artist: '',
+        genre: '',
+        tabSource: '',
+        rating: '',
+      },
+      {
+        tabTitle: 'Red River Valley',
+        tabURL: '/tabs/red-river-valley-gtm.pdf',
+        tuning: '1',
+        difficulty: 'Easy',
+        artist: '',
+        genre: '',
+        tabSource: '',
+        rating: '',
+      }
+    ], 
+    {});
   },
 
   down: async (queryInterface, Sequelize) => {
